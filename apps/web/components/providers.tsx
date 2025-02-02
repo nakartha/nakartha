84ad1ae5eae4d58b08e928297e16e9d67ac8877e
Himanshu <himanshu@begenuin.com>
2025-02-02 18:07:06 +0530

    Revert "Trpc changes (#19)"
    This reverts commit 242ff27515a407e4ef348b79db4f97dd6fbf29f9.

--- apps/web/components/providers.tsx
+++ apps/web/components/providers.tsx
@@ -1,18 +1,9 @@
-"use client";
+"use client"
 
-import * as React from "react";
-import { ThemeProvider as NextThemesProvider } from "next-themes";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { trpc } from "../lib/trpc";
+import * as React from "react"
+import { ThemeProvider as NextThemesProvider } from "next-themes"
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  const [queryClient] = React.useState(() => new QueryClient());
-  const [trpcClient] = React.useState(() =>
-    trpc.createClient({
-      url: "/api/trpc",
-    })
-  );
-
   return (
     <NextThemesProvider
       attribute="class"
@@ -21,11 +12,7 @@ export function Providers({ children }: { children: React.ReactNode }) {
       disableTransitionOnChange
       enableColorScheme
     >
-      <trpc.Provider client={trpcClient} queryClient={queryClient}>
-        <QueryClientProvider client={queryClient}>
-          {children}
-        </QueryClientProvider>
-      </trpc.Provider>
+      {children}
     </NextThemesProvider>
-  );
+  )
 }
